9b9e0e0a29d56ed2595ec1720effd113f030c5e7
krypciak <krypek@tuta.io>
2022-08-08 11:35:32 +0200

    added add-user.sh
    Former-commit-id: 574d9391bcbb899c9cd8f5d984adb60471f1b2b1
    Former-commit-id: a646e23f5a1a1c5ac2ad9eb75595eda0fa2e15d3

---
+++ artix/add-user.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+USER1="krypek"
+
+useradd -m -G tty ftp games network scanner libvirt users video audio wheel -s /bin/zsh $USER1
+echo "Password for $USER1:"
+n=0
+until [ "$n" -ge 5 ]
+do
+   passwd $USER1 && break
+   n=$((n+1)) 
+   sleep 15
+done
+chown -R $USER1:$USER1 /home/$USER1
