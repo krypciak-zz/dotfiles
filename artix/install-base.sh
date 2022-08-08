cea4d0456535e978b57a6d44374d6a8265ee3158
krypciak <krypek@tuta.io>
2022-08-08 11:29:01 +0200

    Update install-base.sh
    Former-commit-id: 52912ab54b438d7536ba312466e480bfcd379502
    Former-commit-id: d59e6ef209540d7dcbeacf7ee4c375f04b72fdf0

--- artix/install-base.sh
+++ artix/install-base.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pacman -Syu neovim networkmanager-openrc archlinux-keyring autoconf automake binutils bison fakeroot file findutils flex gawk gcc gettext grep groff gzip libtool m4 make pacman patch pkgconf sed doas texinfo which git artix-mirrorlist artix-keyring
+pacman -Syu neovim networkmanager-openrc autoconf automake binutils bison fakeroot file findutils flex gawk gcc gettext grep groff gzip libtool m4 make pacman patch pkgconf sed doas texinfo which git artix-mirrorlist artix-keyring
