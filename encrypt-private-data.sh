ebab64e0d8ac25d638b1028d993cc62e3a5c2e63
krypciak <krypek@tuta.io>
2022-08-08 21:18:33 +0200

    added password encrypted password folder
    Former-commit-id: 2be775ad031341e327c7bdd8a54ed9892caf686c [formerly 62c4afb0506472cbed7153b5e38ba2d896ba6d86]
    Former-commit-id: eb175dd123bf79cd4816ce9e0b60cba6a80df512
    Former-commit-id: 38cf84e2444b5b09ab0921044cc59258578de4d7

--- encrypt-private-data.sh
+++ encrypt-private-data.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
+DOTFILES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Prepare gnupg
+GNUPG_DIR=~/.local/share/gnupg
+mkdir -p ~/.local/share/gnupg
+find $GNUPG_DIR -type f -exec chmod 600 {} \; # Set 600 for files
+find $GNUPG_DIR -type d -exec chmod 700 {} \; # Set 700 for directories
+
+
+PRIV_DIR=$DOTFILES_DIR/dotfiles/private
+ENCRYPTED_ARCHIVE=$DOTFILES_DIR/dotfiles/private.tar.gz.gpg
+
+tar -cz $PRIV_DIR | gpg --symmetric --output $ENCRYPTED_ARCHIVE
+sha512sum $ENCRYPTED_ARCHIVE > ${ENCRYPTED_ARCHIVE}.sha512
 
 
