1cc0df43bf0d080670e1e01e18cf3b841f56db12
krypciak <krypek@tuta.io>
2022-08-08 11:22:21 +0200

    Update install-dotfiles-root.sh
    Former-commit-id: c29c004b9ba87bc4d76fb01ce42519eaa777e9b5
    Former-commit-id: e2be2cdbb2f25523ca9a86e95381c75441484f16

--- install-dotfiles-root.sh
+++ install-dotfiles-root.sh
@@ -5,5 +5,6 @@ THISDIR="/home/krypek/.config/dotfiles"
 cp root-files/.zshrc $ROOTDIR/.zshrc
 chown root:root $ROOTDIR/.zshrc
 
+mkdir -p $ROOTFIR/.config
 cp -r root-files/nvim $ROOTDIR/.config/nvim
 chown -R root:root $ROOTDIR/.zshrc
