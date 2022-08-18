456778f2b0b587a5789c605b8a340382b19c77f7
krypciak <krypek@tuta.io>
2022-08-18 23:28:39 +0200

    Update signals.lua
    Former-commit-id: 5135c26d72729a4ffcba462d692dd0968149053e [formerly c807c20bcd1ff44796c147ee636f105f610afba0]
    Former-commit-id: ecd8bc75d7a6b034ed1d710512b8cc8d8ead457b
    Former-commit-id: 53b532a9bfd75751b612869ebe236e4ccce63539

--- dotfiles/awesome/signals.lua
+++ dotfiles/awesome/signals.lua
@@ -71,8 +71,9 @@ client.connect_signal("unfocus",
 -- Screen signals
 --
 -- Create a wibox for each screen and add it
-awful.screen.connect_for_each_screen(
-    function(s) beautiful.at_screen_connect(s) end)
+awful.screen.connect_for_each_screen(function(s) 
+    beautiful.at_screen_connect(s) 
+end)
 -- Screen siglans
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
 screen.connect_signal("property::geometry", function(s)
