2ed13a105807f238373ebeb5780aa25f211e5f0f
krypciak <krypek@tuta.io>
2022-08-28 23:54:17 +0200

    Add bash dotfiles
    Former-commit-id: 57f60eca1d005746dc345673735d5de448045209 [formerly aac6717511c7a87990f704b8bc84686c92681d09]
    Former-commit-id: 874b3e44934fcf0db7fcede6bdebd6f96bc305de
    Former-commit-id: ca1600622c0c269475566f5bae681b91224f72b3

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,7 +1,7 @@
 #~/bin/sh
 
-HOMEDIR="/home/$USER1/home"
-#CONFIGDIR="${HOMEDIR}/.config"
+REAL_HOMEDIR="/home/$USER1"
+HOMEDIR="$REAL_HOMEDIR/home"
 
 DOTFILES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -28,32 +28,54 @@ HARDLINKS_DIRS=(
 	"FreeTube/settings.db"
 )
 
+HOME_DIRS=(
+    ".bashrc"
+    ".bash_profile"
+)
 
-ln -sf $HOMEDIR/.config /home/$USER1/
-ln -sf $HOMEDIR/.local /home/$USER1/
-ln -sf $HOMEDIR/Documents /home/$USER1/
-ln -sf $HOMEDIR/Downloads /home/$USER1/
-ln -sf $HOMEDIR/Pictures /home/$USER1/
-ln -sf $HOMEDIR/Videos /home/$USER1/
-ln -sf $HOMEDIR/Programming /home/$USER1/
-ln -sf $HOMEDIR/VM /home/$USER1/
-ln -sf $HOMEDIR/Games /home/$USER1/
-ln -sf $HOMEDIR/Temp /home/$USER1/
-ln -sf $HOMEDIR/Music /home/$USER1/
+LINK_HOME_DIRS=(
+    ".config"
+    ".local"
+    "Documents"
+    "Downloads"
+    "Pictures"
+    "Videos"
+    "Programming"
+    "VM"
+    "Games"
+    "Temp"
+    "Music"
+)
 
-mkdir -p /home/$USER1/.config/chromium/Default
-mkdir -p /home/$USER1/.config/FreeTube
+mkdir -p $REAL_HOMEDIR
+mkdir -p $HOMEDIR
+
+for dir in "${LINK_HOME_DIRS[@]}"; do
+    mkdir -p $HOMEDIR/$dir
+    ln -sf $HOMEDIR/$dir $REAL_HOMEDIR/
+done
+
+
+mkdir -p $REAL_HOMEDIR/.config/chromium/Default
+mkdir -p $REAL_HOMEDIR/.config/FreeTube
+
+for dir in "${HOME_DIRS[@]}"; do
+    FROM="$DOTFILES_DIR/dotfiles/$dir"
+    DEST="$REAL_HOMEDIR/$dir"
+	ln -sfT "$FROM" "$DEST"
+	chown -R $USER1:$USER1 "$DEST"
+done
 
 for dir in "${SYMLINKS_DIRS[@]}"; do
 	FROM="$DOTFILES_DIR/dotfiles/$dir"
-	DEST="/home/$USER1/.config/$dir"
+	DEST="$REAL_HOMEDIR/.config/$dir"
 	ln -sfT "$FROM" "$DEST"
 	chown -R $USER1:$USER1 "$DEST"
 done
 
 for dir in "${HARDLINKS_DIRS[@]}"; do
 	FROM="$DOTFILES_DIR/dotfiles/$dir"
-	DEST="/home/$USER1/.config/$dir"
+	DEST="$REAL_HOMEDIR/.config/$dir"
 	ln -fT "$FROM" "$DEST"
 	chown -R $USER1:$USER1 "$DEST"
 done
