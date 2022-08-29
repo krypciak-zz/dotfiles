891610e610583e6805c3455f498b5f2993e27f32
krypciak <krypek@tuta.io>
2022-08-29 22:33:56 +0200

    Update install.sh
    Former-commit-id: 0a9a4ba21e80593a5d453345503dd4ee1c7091d7 [formerly 01242859e8c23687bc3c98cd8e09112406ac90c5]
    Former-commit-id: 507d2d5129cd39fff77bf5fe76e2c69293ec19a8
    Former-commit-id: 61365aaf0f52d7ae0a171dcc3a6aad832b2fe129

--- artix/install.sh
+++ artix/install.sh
@@ -124,7 +124,7 @@ cp -rf $DOTFILES_DIR/../ $NEW_DOTFILES_DIR/
 
 pri "Chrooting..."
 confirm "bul" "ignore"
-artix-chroot $INSTALL_DIR 'sh $USER_HOME/.config/dotfiles/artix/after-chroot.sh'
+artix-chroot $INSTALL_DIR "sh $USER_HOME/.config/dotfiles/artix/after-chroot.sh"
 
 confirm "Reboot?" "ignore"
 unmount
