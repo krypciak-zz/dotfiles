b51d915745c5e7afc6a7a09106e9e4ef7fcfeb2d
krypciak <krypek@tuta.io>
2022-08-29 17:25:00 +0200

    Update install.sh
    Former-commit-id: 0fa4a54c137ff06caec93dfb398196b1b1625d59 [formerly 6e6ac7e21c07e094faa370eb78cfed729d421f6b]
    Former-commit-id: d70ee823ad847070a3b99d0ddf4534cc434daa8f
    Former-commit-id: dd6b514f2cdc85d645785e8159cb1c0fde5ea757

--- artix/install.sh
+++ artix/install.sh
@@ -1,8 +1,13 @@
+DISK="/dev/sdX"
+EFI_PART="${DISK}1"
+# In MB
+EFI_SIZE=40
+LVM_PART="${DISK}2"
 
-export DISK="/dev/sdX"
-export EFI_PART="${DISK}2"
-export LVM_PART="${DISK}3"
+export INSTALL_DIR="/mnt/artix"
+EFI_DIR=$INSTALL_DIR/efi
 
+LVM_NAME='artixlvm'
 
 export USER1="krypek"
 export REGION="Europe"
@@ -15,6 +20,59 @@ export DOTFILES_DIR="$USER_HOME/.config/dotfiles"
 export INSTALL_DIR="$DOTFILES_DIR/artix"
 export CONFIGF_DIR="$DOTFILES_DIR/config-files"
 
+LGREEN='\033[1;32m'l
+GREEN='\033[0;32m'l
+RED='\033[0;31m'
+NC='\033[0m' 
 
+function confirm() {
+    read -p "Continue (y/n)?" choice
+    case "$choice" in 
+    y|Y ) echo "yes";;
+    n|N ) exit;;
+    * ) confirm; return;;
+    esac
+}
+
+echo -e "$RED Start partitioning the disk? $RED(DATA WARNING)$NC"
+confirm
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
+  g # set partitioning scheme to GPT
+  o # clear the in memory partition table
+  n # Create EFI partition
+  p # primary partition
+  1 # partition number 1
+    # default - start at beginning of disk 
+  +${EFI_SIZE}M # your size
+  n # Create LVM partition
+  p # primary partition
+  2 # partion number 2
+    # default, start immediately after preceding partition
+    # default, extend partition to end of disk
+  p # print the in-memory partition table
+  w # write the partition table
+  q # and we're done
+EOF
+confirm
+
+# Create encryptred container on LVM_PART
+echo -e "$LGREEN Setting up luks on $LVM_PART $RED(DATA WARNING)$NC"
+cryptsetup luksFormat --key-size 512 --hash sha512 --iter-time 5000 $LVM_PART
+
+echo -e "$LGREEN Opening $LVM_PART as $LVM_NAME $NC"
+cryptsetup open $LVM_PART $LVM_NAME
+
+confirm
+
+# Format EFI_PART
+echo -e"$LGREEN Formatting $EFI_PART as FAT32 $RED(DATA WARNING)$NC"
+mkfs.fat -n efi -F 32 $EFI_PART
+
+# Mount EFI_PART
+echo -e "$GREEN Mounting $EFI_PART to $EFI_DIR $NC"
+mkdir -p $EFI_DIR
+mount $EFI_PART $EFI_DIR
+
+confirm
 
 
