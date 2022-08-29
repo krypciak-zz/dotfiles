5350a5059eff49663766ec1f43365db4f7241047
krypciak <krypek@tuta.io>
2022-08-29 23:50:53 +0200

    Artix install update
    Former-commit-id: a47758c14b3852448def4a09d23d8fa0e355cd56 [formerly 400d50b4fa400dacb91397f712c9d213f4a4d7b0]
    Former-commit-id: e548b206247e418cdf0c03708ab443e893ea7062
    Former-commit-id: 5c07b7a57f3cd081043852186f984fbc8f8d6283

--- artix/install-base.sh
+++ artix/install-base.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-pacman --needed --noconfirm -Syu neovim networkmanager-openrc autoconf automake binutils bison fakeroot file findutils flex gawk gcc gettext grep groff gzip libtool m4 make pacman patch pkgconf sed opendoas texinfo which git artix-mirrorlist artix-keyring wget cargo iptables-nft lsd lvm2 linux-lts linux-firmware system/mkinitcpio
+pacman --needed --noconfirm -Syu neovim networkmanager-openrc autoconf automake binutils bison fakeroot file findutils flex gawk gcc gettext grep groff gzip libtool m4 make pacman patch pkgconf sed opendoas texinfo which git artix-mirrorlist artix-keyring wget cargo lvm2 linux-lts linux-firmware system/mkinitcpio world/rust
 
