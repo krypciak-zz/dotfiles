adf2f277615879a1e24fa1bb3c54041135a37b6f
krypciak <krypek@tuta.io>
2022-08-29 17:24:45 +0200

    Remove zsh from root files
    Former-commit-id: 1e85e9795c51b81de15cdae3bf645d97a83376b0 [formerly ad9d06bc13cfc4542038397f87109b9780852543]
    Former-commit-id: 01107ff2c2709421dd64ef703d4b750196899c46
    Former-commit-id: 725d73f3f6ca3e5a4a16af2962eee57417c9c894

--- install-dotfiles-root.sh
+++ install-dotfiles-root.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-
 DOTFILES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-cp $DOTFILES_DIR/dotfiles/root-files/.zshrc /root/.zshrc
-chown root:root /root/.zshrc
 
 mkdir -p /root/.config/nvim
 cp -r $DOTFILES_DIR/dotfiles/root-files/nvim /root/.config/
