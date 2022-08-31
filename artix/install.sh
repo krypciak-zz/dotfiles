c1c6484f485243bd1e0653d662962ef745812da3
krypciak <krypek@tuta.io>
2022-08-31 16:11:36 +0200

    Update install.sh
    Former-commit-id: 1ebd0b6cfd651cc675cf04fd5c17a867fb708870 [formerly 287d79b5175f782f01247c70f45323409ed9fd47]
    Former-commit-id: 11c1314712dbb59dd6bd0428105169cd60801a18
    Former-commit-id: d626a988bfc9d3c4ec410aae60ac883ddc029696

--- artix/install.sh
+++ artix/install.sh
@@ -114,7 +114,7 @@ swapon $LVM_DIR/swap
 # Prepare to chroot
 confirm "Basestrap basic packages?"
 export LANG
-basestrap -C $ARTIXD_DIR/../config-files/pacman.conf.install $INSTALL_DIR base openrc elogind-openrc linux-firmware $KERNEL $KERNEL-headers iptables-nft artix-keyring artix-mirrorlist mkinitcpio
+basestrap -C $ARTIXD_DIR/../config-files/pacman.conf.install $INSTALL_DIR base openrc elogind-openrc linux-firmware $KERNEL $KERNEL-headers iptables-nft artix-keyring artix-mirrorlist mkinitcpio btrfs-progs
 
 pri "Generating fstab"
 fstabgen -U $INSTALL_DIR >> $INSTALL_DIR/etc/fstab
