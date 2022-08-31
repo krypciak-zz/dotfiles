5739197a4f9bd2de46a0b61196d833ea433f34fc
krypciak <krypek@tuta.io>
2022-08-31 13:12:28 +0200

    Artix install update
    Former-commit-id: 0791ca9d73eb948a94ff9954fafdcda792cc3b46 [formerly cd16e73827f7c7c380fe66a143a94f9b9d7d27b7]
    Former-commit-id: 055fbddf6f73cf05c5d49397a7005f54f5e57f6e
    Former-commit-id: f970edc759ff5c23a4b3793a6bb3a8c26bd44cd0

--- artix/packages/install-awesome.sh
+++ artix/packages/install-awesome.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 function install_awesome() {
-    echo 'alacritty awesome breeze breeze-gtk breeze-icons dmenu feh lxappearance qt5-base qt6-base unclutter-xfixes-git xbindkeys xdg-ninja-git xdg-desktop-portal xdotool xkeyboard-config xorg-server xorg-xinit xorg-xprop xorg-xrandr xorg-xrefresh zenity copyq world/xorg-xmodmap redshift scrot'
+    echo 'alacritty awesome breeze breeze-gtk breeze-icons dmenu feh lxappearance qt5-base qt6-base unclutter-xfixes-git xbindkeys xdg-ninja-git xdg-desktop-portal xdotool xkeyboard-config xorg-server xorg-xinit xorg-xprop xorg-xrandr xorg-xrefresh zenity copyq world/xorg-xmodmap redshift scrot topgrade '
 }
