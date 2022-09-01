afa1107aff3b0c6338f2f4a540a51bdace10dc0c
krypciak <krypek@tuta.io>
2022-09-01 21:13:50 +0200

    Update after-chroot.sh
    Former-commit-id: a51f8445672f7f828f4c9694dba307f5af0386cc [formerly 00fb12e6f43e7f36a024860898e057d4a33e880e]
    Former-commit-id: 98e81d7e8e6edd6b4c28cd5675fc016a35ba8418
    Former-commit-id: 2e050c9e90dc2e975cac41fad86003abf3c5d2a3

--- artix/after-chroot.sh
+++ artix/after-chroot.sh
@@ -48,10 +48,12 @@ pri "Updating keyring"
 # Disable package signature verification
 sed -i 's/SigLevel    = Required DatabaseOptional/SigLevel = Never/g' /etc/pacman.conf
 sed -i 's/LocalFileSigLevel = Optional/#LocalFileSigLevel = Optional/g' /etc/pacman.conf
+# Add lib32 repo
+printf '[lib32]\nInclude = /etc/pacman.d/mirrorlist\n' >> /etc/pacman.conf
 # Add universe repo
 printf '[universe]\nServer = https://universe.artixlinux.org/$arch\nServer = https://mirror1.artixlinux.org/universe/$arch\nServer = https://mirror.pascalpuffke.de/artix-universe/$arch\nServer = https://artixlinux.qontinuum.space/artixlinux/universe/os/$arch\nServer = https://mirror1.cl.netactuate.com/artix/universe/$arch\nServer = https://ftp.crifo.org/artix-universe/' >> /etc/pacman.conf
 
-pacman $PACMAN_ARGUMENTS -Sy artix-archlinux-support
+pacman $PACMAN_ARGUMENTS -Sy artix-archlinux-support lib32-artix-archlinux-support
 pacman-key --init
 pacman-key --populate
 pri "Copying pacman configuration"
