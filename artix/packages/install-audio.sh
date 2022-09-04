bf2706e5d78c7754c45bb19bf63b37c0ea8efb96
krypciak <krypek@tuta.io>
2022-09-04 19:51:32 +0200

    Update install-audio.sh
    Former-commit-id: bab99fe293069a51018df7ffbf128cb02b354ec5 [formerly 3807490c7f9f2be91006c4eab323fd1346102d8c]
    Former-commit-id: 6f8f38847e6bfe3139711b69be3a1b03bfd8c841
    Former-commit-id: 200ccbeb91859b3be1bfb7513ed9abc0cdf57266

--- artix/packages/install-audio.sh
+++ artix/packages/install-audio.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 function install_audio() {
-    echo 'alsa-firmware alsa-lib alsa-plugins alsa-utils alsa-utils-openrc apulse media-player-info pulseaudio world/pipewire-jack world/webrtc-audio-processing cmus-git playerctl'
+    echo 'alsa-firmware alsa-lib alsa-plugins alsa-utils alsa-utils-openrc apulse cmus-git playerctl'
 }
+# world/webrtc-audio-processing media-player-info pulseaudio world/pipewire-jack 
