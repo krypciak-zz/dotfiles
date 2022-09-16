fcbf0dc016237d465c05cb85427a09336745b75d
krypek <75359441+krypciak@users.noreply.github.com>
2022-09-16 15:45:09 +0200

    Fix league of legends

--- artix/packages/install-gaming.sh
+++ artix/packages/install-gaming.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 function install_gaming() {
-    echo 'lutris steam jdk17-openjdk jdk8-openjdk multimc-bin jdk-openjdk world/jre-openjdk world/jre-openjdk-headless wine-staging bottles gamemode'
+    echo 'lutris steam jdk17-openjdk jdk8-openjdk multimc-bin jdk-openjdk world/jre-openjdk world/jre-openjdk-headless wine-staging bottles gamemode lib32-giflib lib32-mpg123 lib32-openal lib32-v4l-utils lib32-libpulse lib3
+2-libxcomposite lib32-libxinerama lib32-opencl-icd-loader'
 }
